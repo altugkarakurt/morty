be0ebd82e8e126a1e7e5e2654cce8dac17bf1848
Sertan Senturk <contact@sertansenturk.com>
2015-07-24 00:21:16 +0200

    Reverted the indexing for trainBozkurt to 1:2500 for paralel processing

--- OptimizationExperiments/trainBozkurt_wrapper.sh
+++ OptimizationExperiments/trainBozkurt_wrapper.sh
@@ -20,7 +20,7 @@
 #
 # Create an array job = !!!!!!number of audio in the target folder!!!!!!
 # ----------------
-#$ -t 1-29:1
+#$ -t 1-2500:1
 #
 # Send me a mail when processed and when finished:
 # ------------------------------------------------
