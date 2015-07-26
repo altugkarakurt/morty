1e22fcdb48c2bcad0a87f54d742bcbba355f348e
Sertan Senturk <contact@sertansenturk.com>
2015-07-26 14:45:21 +0200

    Changed the bin generation in pd such that the bins always cross 0

--- ModeFunctions.py
+++ ModeFunctions.py
@@ -8,31 +8,40 @@ from scipy.spatial import distance
 
 import PitchDistribution as p_d
 
+
 def load_track(txt_name, txt_dir):
 	"""---------------------------------------------------------------------------------------
-	Loads the pitch track from a text file. The format for the examples is, such that, 0th
-	column is time-stamps and 1st column is the corresponding frequency values.
-	---------------------------------------------------------------------------------------"""
+    Loads the pitch track from a text file. The format for the examples is, such that, 0th
+    column is time-stamps and 1st column is the corresponding frequency values.
+    ---------------------------------------------------------------------------------------"""
 	return np.loadtxt(os.path.join(txt_dir, txt_name))
 
-def generate_pd(pitch_track, ref_freq=440, smooth_factor=7.5, cent_ss=7.5, source='', segment='all', overlap='-'):		
-	### Some extra interval is added to the beginning and end since the 
+
+def generate_pd(pitch_track, ref_freq=440, smooth_factor=7.5, cent_ss=7.5, source='', segment='all', overlap='-'):
+	### Some extra interval is added to the beginning and end since the
 	### superposed Gaussian for smoothing would vanish after 3 sigmas.
 	### The limits are also quantized to be a multiple of chosen step-size
 	### smooth_factor = standard deviation fo the gaussian kernel
-	if(smooth_factor > 0):
-		smoothening = (smooth_factor * np.sqrt(1/np.cov(pitch_track)))
-		min_bin = (min(pitch_track) - (min(pitch_track) % smooth_factor)) - (5 * smooth_factor)  
+	if (smooth_factor > 0):
+		smoothening = (smooth_factor * np.sqrt(1 / np.cov(pitch_track)))
+		min_bin = (min(pitch_track) - (min(pitch_track) % smooth_factor)) - (5 * smooth_factor)
 		max_bin = (max(pitch_track) + (smooth_factor - (max(pitch_track) % smooth_factor))) + (5 * smooth_factor)
 
-		pd_bins = np.arange(min_bin, max_bin, cent_ss)
+		# generate the pitch distribution bins; make sure it crosses 0
+		pd_bins = np.concatenate([np.arange(0, min_bin, -cent_ss)[::-1], np.arange(cent_ss, max_bin, cent_ss)])
+
+		# generate the kernel density estimate and evaluate at the given bins
 		kde = stats.gaussian_kde(pitch_track, bw_method=smoothening)
 		pd_vals = kde.evaluate(pd_bins)
 	else:
 		pd_bins = np.arange((min(pitch_track)), max(pitch_track), cent_ss)
-		pd_vals = np.histogram(pitch_track, bins=np.arange((min(pitch_track) - (cent_ss/2)), (max(pitch_track) + (cent_ss/2)), cent_ss), density=True)[0]
-		pd_vals[0] = 0 #filter out the pitch extraction errors
-	return p_d.PitchDistribution(pd_bins[1:], pd_vals, kernel_width=smooth_factor, source=source, ref_freq=ref_freq, segment=segment, overlap=overlap)
+		pd_vals = np.histogram(pitch_track,
+		                       bins=np.arange((min(pitch_track) - (cent_ss / 2)), (max(pitch_track) + (cent_ss / 2)),
+		                                      cent_ss), density=True)[0]
+		pd_vals[0] = 0  # filter out the pitch extraction errors
+	return p_d.PitchDistribution(pd_bins[1:], pd_vals, kernel_width=smooth_factor, source=source, ref_freq=ref_freq,
+	                             segment=segment, overlap=overlap)
+
 
 def generate_pcd(pd):
 	### Initializations
@@ -43,8 +52,10 @@ def generate_pcd(pd):
 	for k in range(len(pd.bins)):
 		idx = int((pd.bins[k] % 1200) / pd.step_size)
 		pcd_vals[idx] = pcd_vals[idx] + pd.vals[k]
-		
-	return p_d.PitchDistribution(pcd_bins, pcd_vals, kernel_width=pd.kernel_width, source=pd.source, ref_freq=pd.ref_freq, segment=pd.segmentation, overlap=pd.overlap)
+
+	return p_d.PitchDistribution(pcd_bins, pcd_vals, kernel_width=pd.kernel_width, source=pd.source,
+	                             ref_freq=pd.ref_freq, segment=pd.segmentation, overlap=pd.overlap)
+
 
 def hz_to_cent(hertz_track, ref_freq):
 	### Hertz-to-Cent Conversion. Since the log of zeros are non_defined,
@@ -52,121 +63,130 @@ def hz_to_cent(hertz_track, ref_freq):
 	filtered_track = [freq for freq in hertz_track if freq > 0]
 	cent_track = []
 	for freq in filtered_track:
-		cent_track.append(math.log((freq/ref_freq), 2.0) * 1200.0)
+		cent_track.append(math.log((freq / ref_freq), 2.0) * 1200.0)
 	return cent_track
-	
+
+
 def cent_to_hz(cent_track, ref_freq):
 	hertz_track = []
 	for cent in cent_track:
 		hertz_track.append((2 ** (cent / 1200.0)) * ref_freq)
 	return hertz_track
 
+
 def generate_distance_matrix(dist, peak_idxs, mode_dists, method='euclidean'):
 	"""---------------------------------------------------------------------------------------
-	Iteratively calculates the distance for all candidate tonics and candidate modes of a piece.
-	The pair of candidates that give rise to the minimum value in this matrix is chosen as the
-	estimate by the higher level functions.
-	---------------------------------------------------------------------------------------"""
+    Iteratively calculates the distance for all candidate tonics and candidate modes of a piece.
+    The pair of candidates that give rise to the minimum value in this matrix is chosen as the
+    estimate by the higher level functions.
+    ---------------------------------------------------------------------------------------"""
 	result = np.zeros((len(peak_idxs), len(mode_dists)))
-	for i in range(len(peak_idxs)): 
+	for i in range(len(peak_idxs)):
 		trial = dist.shift(peak_idxs[i])
 		for j in range(len(mode_dists)):
 			result[i][j] = distance(trial, mode_dists[j], method=method)
 	return np.array(result)
 
+
 def distance(piece, trained, method='euclidean'):
 	"""---------------------------------------------------------------------------------------
-	Calculates the distance metric between two pitch distributions. This is called from
-	estimation functions.
-	---------------------------------------------------------------------------------------"""
-	if(method=='euclidean'):
+    Calculates the distance metric between two pitch distributions. This is called from
+    estimation functions.
+    ---------------------------------------------------------------------------------------"""
+	if (method == 'euclidean'):
 		return sp.spatial.distance.euclidean(piece.vals, trained.vals)
 
-	elif(method=='manhattan'):
+	elif (method == 'manhattan'):
 		return sp.spatial.distance.minkowski(piece.vals, trained.vals, 1)
 
-	elif(method=='l3'):
+	elif (method == 'l3'):
 		return sp.spatial.distance.minkowski(piece.vals, trained.vals, 3)
-			
-	elif(method=='bhat'):
+
+	elif (method == 'bhat'):
 		d = 0
 		for i in range(len(piece.vals)):
 			d += math.sqrt(piece.vals[i] * trained.vals[i]);
 		return (-math.log(d))
 
-	elif(method=='intersection'):
+	elif (method == 'intersection'):
 		d = 0
 		for j in range(len(piece.vals)):
 			d += min(piece.vals[i], trained.vals[i])
 		return d / (len(piece.vals))
 
-	elif(method=='corr'):
+	elif (method == 'corr'):
 		return 1.0 / np.correlate(piece.vals, trained.vals)
 
 	else:
 		return 0
 
+
 def pd_zero_pad(pd, mode_pd, cent_ss=7.5):
 	"""---------------------------------------------------------------------------------------
-	This function is only used while detecting tonic and working with pd as metric. It pads
-	zeros from both sides of the values array to avoid losing non-zero values when comparing
-	and to make sure the two PDs are of the same length 
-	---------------------------------------------------------------------------------------"""
+    This function is only used while detecting tonic and working with pd as metric. It pads
+    zeros from both sides of the values array to avoid losing non-zero values when comparing
+    and to make sure the two PDs are of the same length
+    ---------------------------------------------------------------------------------------"""
 	### Alignment of the left end-points
-	if((min(pd.bins) - min(mode_pd.bins)) > 0):
+	if ((min(pd.bins) - min(mode_pd.bins)) > 0):
 		temp_left_shift = (min(pd.bins) - min(mode_pd.bins)) / cent_ss
 		pd.vals = np.concatenate((np.zeros(temp_left_shift), pd.vals))
-	elif((min(pd.bins) - min(mode_pd.bins)) < 0):
+	elif ((min(pd.bins) - min(mode_pd.bins)) < 0):
 		mode_left_shift = (min(mode_pd.bins) - min(pd.bins)) / cent_ss
 		mode_pd.vals = np.concatenate((np.zeros(mode_left_shift), mode_pd.vals))
 
 	### Alignment of the right end-points
-	if((max(pd.bins) - max(mode_pd.bins)) > 0):
+	if ((max(pd.bins) - max(mode_pd.bins)) > 0):
 		mode_right_shift = (max(pd.bins) - max(mode_pd.bins)) / cent_ss
 		mode_pd.vals = np.concatenate((mode_pd.vals, np.zeros(mode_right_shift)))
-	elif((max(mode_pd.bins) - max(pd.bins)) > 0):    
+	elif ((max(mode_pd.bins) - max(pd.bins)) > 0):
 		temp_right_shift = (max(mode_pd.bins) - max(pd.bins)) / cent_ss
 		pd.vals = np.concatenate((pd.vals, (np.zeros(temp_right_shift))))
 
 	return pd, mode_pd
 
+
 def tonic_estimate(dist, peak_idxs, mode_dist, distance_method="euclidean", metric='pcd', cent_ss=7.5):
 	"""---------------------------------------------------------------------------------------
-	Given the mode (or candidate mode), compares the piece's distribution using the candidate
-	tonics and returns the resultant distance vector to higher level functions.
-	---------------------------------------------------------------------------------------"""
+    Given the mode (or candidate mode), compares the piece's distribution using the candidate
+    tonics and returns the resultant distance vector to higher level functions.
+    ---------------------------------------------------------------------------------------"""
 	### Mode is known, tonic is estimated.
 	### Piece's distributon is generated
-	
-	if(metric=='pcd'):
-		return np.array(generate_distance_matrix(dist, peak_idxs, [mode_dist], method=distance_method))[:,0]
 
-	elif(metric=='pd'):
-		temp = p_d.PitchDistribution(dist.bins, dist.vals, kernel_width=dist.kernel_width, source=dist.source, ref_freq=dist.ref_freq, segment=dist.segmentation)
+	if (metric == 'pcd'):
+		return np.array(generate_distance_matrix(dist, peak_idxs, [mode_dist], method=distance_method))[:, 0]
+
+	elif (metric == 'pd'):
+		temp = p_d.PitchDistribution(dist.bins, dist.vals, kernel_width=dist.kernel_width, source=dist.source,
+		                             ref_freq=dist.ref_freq, segment=dist.segmentation)
 		temp, mode_dist = pd_zero_pad(temp, mode_dist, cent_ss=cent_ss)
 
 		### Filling both sides of vals with zeros, to make sure that the shifts won't drop any non-zero values
 		temp.vals = np.concatenate((np.zeros(abs(max(peak_idxs))), temp.vals, np.zeros(abs(min(peak_idxs)))))
 		mode_dist.vals = np.concatenate((np.zeros(abs(max(peak_idxs))), mode_dist.vals, np.zeros(abs(min(peak_idxs)))))
-			
-		return np.array(generate_distance_matrix(temp, peak_idxs, [mode_dist], method=distance_method))[:,0]
+
+		return np.array(generate_distance_matrix(temp, peak_idxs, [mode_dist], method=distance_method))[:, 0]
+
 
 def mode_estimate(dist, mode_dists, distance_method='euclidean', metric='pcd', cent_ss=7.5):
 	"""---------------------------------------------------------------------------------------
-	Given the tonic (or candidate tonic), compares the piece's distribution using the candidate
-	modes and returns the resultant distance vector to higher level functions.
-	---------------------------------------------------------------------------------------"""
+    Given the tonic (or candidate tonic), compares the piece's distribution using the candidate
+    modes and returns the resultant distance vector to higher level functions.
+    ---------------------------------------------------------------------------------------"""
 
-	if(metric=='pcd'):
+	if (metric == 'pcd'):
 		distance_vector = np.array(generate_distance_matrix(dist, [0], mode_dists, method=distance_method))
 
-	elif(metric=='pd'):
+	elif (metric == 'pd'):
 		distance_vector = np.zeros(len(mode_dists))
 		for i in range(len(mode_dists)):
-			trial = p_d.PitchDistribution(dist.bins, dist.vals, kernel_width=dist.kernel_width, source=dist.source, ref_freq=dist.ref_freq, segment=dist.segmentation)
+			trial = p_d.PitchDistribution(dist.bins, dist.vals, kernel_width=dist.kernel_width, source=dist.source,
+			                              ref_freq=dist.ref_freq, segment=dist.segmentation)
 			trial, mode_trial = pd_zero_pad(trial, mode_dists[i], cent_ss=cent_ss)
 			distance_vector[i] = distance(trial, mode_trial, method=distance_method)
 	return distance_vector
+
 
 def slice(time_track, pitch_track, pt_source, chunk_size, threshold=0.5, overlap=0):
 	segments = []
@@ -174,13 +194,14 @@ def slice(time_track, pitch_track, pt_source, chunk_size, threshold=0.5, overlap
 	last = 0
 	for k in np.arange(1, (int(max(time_track) / chunk_size) + 1)):
 		cur = 1 + max(np.where(time_track < chunk_size * k)[0])
-		segments.append(pitch_track[last:(cur-1)])
-		seg_lims.append((pt_source, int(round(time_track[last])), int(round(time_track[cur-1])))) #0 - source, 1 - init, 2 - final
+		segments.append(pitch_track[last:(cur - 1)])
+		seg_lims.append((pt_source, int(round(time_track[last])),
+		                 int(round(time_track[cur - 1]))))  # 0 - source, 1 - init, 2 - final
 		last = 1 + max(np.where(time_track < chunk_size * k * (1 - overlap))[0]) if (overlap > 0) else cur
-	if((max(time_track) - time_track[last]) >= (chunk_size * threshold)):
+	if ((max(time_track) - time_track[last]) >= (chunk_size * threshold)):
 		segments.append(pitch_track[last:])
 		seg_lims.append((pt_source, int(round(time_track[last])), int(round(time_track[len(time_track) - 1]))))
-	elif(last==0):	#If the runtime of the track is below the threshold, keep it as it is
+	elif (last == 0):  # If the runtime of the track is below the threshold, keep it as it is
 		segments.append(pitch_track)
 		seg_lims.append((pt_source, 0, int(round(time_track[len(time_track) - 1]))))
 	return segments, seg_lims
