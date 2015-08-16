b72b5351f9fa14546c36f43b87a4bc8d53f041cf
altug <altugkarakurt@gmail.com>
2015-08-16 22:10:32 +0300

    Added

--- Examples/pitch_track_generator.py
+++ Examples/pitch_track_generator.py
@@ -6,13 +6,17 @@ import numpy as np
 
 """---------------------------------------------------------------------------------------
 Example script for extracting predominant melody using CompMusic's pitch.py. Any other 
-pitch extraction can also be used, as long as their output are of the same format.
+pitch extraction can also be used, as long as their output is of the same format.
 ---------------------------------------------------------------------------------------"""
 extractor = pitch.PitchExtractMakam()
-test_songs = ['2-03_Ussak_Sazsemaisi']
-
-for song in test_songs:
-	results = extractor.run(song + '.mp3')
+recordings = ['2-03_Ussak_Sazsemaisi']
+save_dir = os.path.join('Pitch_Tracks', (recording + '.pitch'))
+for recording in recordings:
+	results = extractor.run(recording + '.mp3')
 	pitch_track = np.array(json.loads(results['pitch']))[:, [0, 1]]
-	with open(('./Pitch Tracks/' + song + '.txt'), 'w') as f:
-		np.savetxt(f, pitch_track)
+	pitch_track = (np.around([i*math.pow(10,DECIMAL) for i in pitch_track[:,1]]) / 100.0).tolist()
+
+	with open(save_dir, 'w') as f:
+		for i in pitch_track:
+			f.write("%.2f\n" % i)
+
