b13966686d7dfb0f8a8c51d0b7ffe6bb16cf9a61
Sertan Senturk <contact@sertansenturk.com>
2015-09-16 16:37:31 +0200

    stratified_fold now also add the tonic values

--- extras/foldGeneration.py
+++ extras/foldGeneration.py
@@ -4,23 +4,34 @@ from extras import fileOperations
 from sklearn import cross_validation
 
 
-def stratified_fold(data_dir, n_folds=10, savefile=''):
+def stratified_fold(data_dir, annotation_file, n_folds=10, savefile=''):
 	modes = fileOperations.getModeNames(data_dir)
-	[filepaths, basefolders] = fileOperations.getFileNamesInDir(data_dir, extension='.pitch')[0: 2]
+	[filepaths, basefolders, filenames] = fileOperations.getFileNamesInDir(data_dir, extension='.pitch')
+
+	with open(annotation_file, 'r') as a:
+		annotations = json.load(a)
 
 	filemodes = [os.path.basename(b) for b in basefolders]
-	mode_idx = [modes.index(m) for m in filemodes]
+	mbids = [os.path.splitext(f)[0] for f in filenames]
+	tonics = []
+	for m in mbids:
+		for a in annotations:
+			if a['mbid'] == m:
+				tonics.append(a['tonic'])
 
 	# get the stratified folds
+	mode_idx = [modes.index(m) for m in filemodes]
 	skf = cross_validation.StratifiedKFold(mode_idx, n_folds=n_folds)
 
 	folds = dict()
 	for ff, fold in enumerate(skf):
 		folds['fold' + str(ff)] = {'train': [], 'test': []}
 		for tr_idx in fold[0]:
-			folds['fold' + str(ff)]['train'].append({'file': filepaths[tr_idx], 'mode': filemodes[tr_idx]})
+			folds['fold' + str(ff)]['train'].append({'file': filepaths[tr_idx], 'mode': filemodes[tr_idx],
+			                                         'tonic': tonics[tr_idx], 'mbid': mbids[tr_idx]})
 		for te_idx in fold[1]:
-			folds['fold' + str(ff)]['test'].append({'file': filepaths[te_idx], 'mode': filemodes[te_idx]})
+			folds['fold' + str(ff)]['test'].append({'file': filepaths[te_idx], 'mode': filemodes[te_idx],
+			                                         'tonic': tonics[te_idx], 'mbid': mbids[te_idx]})
 
 	# save the folds to a file if specified
 	if savefile:
