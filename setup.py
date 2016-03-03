e911a1bc941725dc81ef1469171fa3a61f5a75e1
Sertan Senturk <contact@sertansenturk.com>
2016-03-03 11:39:59 +0100

    Added pip installation and requirement

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='modetonicestimation',
+      version='1.0',
+      author='Altug Karakurt',
+      author_email='altugkarakurt AT gmail DOT com',
+      license='agpl 3.0',
+      description='Python library for mode and tonic estimation.',
+      url='https://github.com/altugkarakurt/ModeTonicEstimation',
+      packages=['modetonicestimation'],
+      include_package_data=True,
+)
