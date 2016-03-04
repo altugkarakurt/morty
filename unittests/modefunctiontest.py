88476fec9422b2e2945c15a45b90091755d88dfc
Sertan Senturk <contact@sertansenturk.com>
2016-03-04 23:16:46 +0100

    Fixed several bugs in unittests/modefunctiontest.py

--- unittests/modefunctiontest.py
+++ unittests/modefunctiontest.py
@@ -1,14 +1,12 @@
 import numpy as np
-import sys
+from modetonicestimation import ModeFunctions as mF
+from modetonicestimation.PitchDistribution import PitchDistribution
 
-sys.path.append('../modetonicestimation')
-import ModeFunctions as mf
-from PitchDistribution import PitchDistribution
 
-def test_generate_pcd:
-	pitch_track = np.loadtxt('pitch_track.txt')
-	pd = mf.generate_pd(pitch_track)
-	pcd = pd = mf.generate_pcd(pd)
-	test_pd = PitchDistribution.load('test_pd')
-	test_pcd = PitchDistribution.load('test_pcd')
-	assert ((pd == test_pd) and (pcd == test_pcd))
+def test_generate_pcd():
+    pitch_track = np.loadtxt('pitch_track.txt')
+    pd = mF.generate_pd(pitch_track)
+    pcd = pd = mF.generate_pcd(pd)
+    test_pd = PitchDistribution.load('test_pd')
+    test_pcd = PitchDistribution.load('test_pcd')
+    assert ((pd == test_pd) and (pcd == test_pcd))
