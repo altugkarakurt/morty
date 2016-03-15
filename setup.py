2acb0f90f40cb50aecb45fe4f65a7b28defeff1e
Sertan Senturk <contact@sertansenturk.com>
2016-03-15 13:51:49 +0100

    Carried the numpy scipy requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 from setuptools import setup
 
 setup(name='modetonicestimation',
-      version='1.0',
-      author='Altug Karakurt',
-      author_email='altugkarakurt AT gmail DOT com',
-      license='agpl 3.0',
-      description='Python library for mode and tonic estimation.',
-      url='https://github.com/altugkarakurt/ModeTonicEstimation',
-      packages=['modetonicestimation'],
-      include_package_data=True,
+version='1.0',
+    author='Altug Karakurt',
+    author_email='altugkarakurt AT gmail DOT com',
+    license='agpl 3.0',
+    description='Python library for mode and tonic estimation.',
+    url='https://github.com/altugkarakurt/ModeTonicEstimation',
+    packages=['modetonicestimation'],
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "scipy"
+    ],
 )
