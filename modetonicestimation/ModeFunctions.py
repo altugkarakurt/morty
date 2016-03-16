9528f44205c66bcae4acd590a4e6626ae6def890
Sertan Senturk <contact@sertansenturk.com>
2016-03-17 00:47:27 +0100

    Removed the conversion functions from modefunctions (they are in Converter)

--- modetonicestimation/ModeFunctions.py
+++ modetonicestimation/ModeFunctions.py
@@ -39,32 +39,6 @@ def parse_pitch_track(pitch_track, multiple=False):
             return np.array(pitch_track)
 
 
-def hz_to_cent(hz_track, ref_freq):
-    """------------------------------------------------------------------------
-    Converts an array of Hertz values into cents.
-    ---------------------------------------------------------------------------
-    hz_track : The 1-D array of Hertz values
-    ref_freq    : Reference frequency for cent conversion
-    ------------------------------------------------------------------------"""
-    hz_track = np.array(hz_track)
-
-    # The 0 Hz values are removed, not only because they are meaningless,
-    # but also logarithm of 0 is problematic.
-    return np.log2(hz_track[hz_track > 0] / ref_freq) * 1200.0
-
-
-def cent_to_hz(cent_track, ref_freq):
-    """------------------------------------------------------------------------
-    Converts an array of cent values into Hertz.
-    ---------------------------------------------------------------------------
-    cent_track  : The 1-D array of cent values
-    ref_freq    : Reference frequency for cent conversion
-    ------------------------------------------------------------------------"""
-    cent_track = np.array(cent_track)
-
-    return 2 ** (cent_track / 1200.0) * ref_freq
-
-
 def generate_distance_matrix(dist, peak_idxs, mode_dists, method='euclidean'):
     """------------------------------------------------------------------------
     Iteratively calculates the distance of the input distribution from each
