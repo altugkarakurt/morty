1162ce6ab5fdd2d039409090051c3c22c6e125ad
altug <altugkarakurt@gmail.com>
2016-04-18 12:06:25 +0300

    PEP8 Fix to Neural Files

--- morty/NeuralClassifier.py
+++ morty/NeuralClassifier.py
@@ -4,7 +4,7 @@ import ModeFunctions as ModeFun
 from PitchDistribution import PitchDistribution
 import numpy as np
 import os
-import pdb
+
 
 class NeuralClassifier:
     def __init__(self, step_size, smooth_factor, mode_names, weights=None):
@@ -14,36 +14,36 @@ class NeuralClassifier:
         self.smooth_factor = smooth_factor
         self.mode_dict = dict()
         for idx, mode in enumerate(mode_names):
-        	self.mode_dict[mode] = [1 if(i == idx) else 0 for i in range(len(mode_names))]
-        
+            self.mode_dict[mode] = [1 if(i == idx) else 0
+                                    for i in range(len(mode_names))]
 
-    def train(self, mode_labels, pitch_files, tonic_freqs, learn_rate, save_dir=''):
+    def train(self, mode_labels, pitch_files, tonic_freqs, learn_rate,
+              save_dir=''):
         tonic_freqs = [np.array(tonic) for tonic in tonic_freqs]
         pitch_tracks = ModeFun.parse_pitch_track(pitch_files, multiple=True)
-        pdb.set_trace()
-        cent_tracks = [Converter.hz_to_cent(k, ref_freq=tonic) \
-                       for k in pitch_tracks]
-        pcd_vals = [PitchDistribution.from_cent_pitch(track, 
-                      tonic_freqs[idx], self.smooth_factor, 
-                      self.step_size).to_pcd().vals \
-                      for idx, track in enumerate(cent_tracks)]
+        cent_tracks = [Converter.hz_to_cent(k, ref_freq=tonic_freqs[idx])
+                       for idx, k in enumerate(pitch_tracks)]
+        pcd_vals = [PitchDistribution.from_cent_pitch(track,
+                    tonic_freqs[idx], self.smooth_factor,
+                    self.step_size).to_pcd().vals
+                    for idx, track in enumerate(cent_tracks)]
         self.train_from_pcds(mode_labels, pcd_vals, learn_rate, save_dir)
-    
+
     def train_from_pcds(self, mode_labels, pcd_list, learn_rate, save_dir=""):
         labels = [self.mode_dict[mode] for mode in mode_labels]
-    	self.neural_net.train(pcd_list, labels, learn_rate=learn_rate)
-        
+        self.neural_net.train(pcd_list, labels, learn_rate=learn_rate)
+
         if(save_dir):
-        	fpath = os.path.join(save_dir, "weights.txt")
-        	np.savetxt(fpath, self.neural_net.weights)
-        	
+            fpath = os.path.join(save_dir, "weights.txt")
+            np.savetxt(fpath, self.neural_net.weights)
+
     def mode_estimate(self, pitch_file, tonic_freq):
-    	pitch_track = ModeFun.parse_pitch_track(pitch_file, multiple=False)
-    	cent_track = Converter.hz_to_cent(pitch_track, ref_freq=tonic_freq)
-    	pcd_vals = PitchDistribution.from_cent_pitch(cent_track, 
-                      tonic_freq, self.smooth_factor, 
-                      self.step_size).to_pcd().vals
-    	mode_est = self.neural_net.estimate(pcd_vals)
-    	max_idx = np.argmax(mode_est)
-    	return [1 if(idx==max_idx) else 0 for idx, _ in enumerate(mode_est)]
-    	
+        pitch_track = ModeFun.parse_pitch_track(pitch_file, multiple=False)
+        cent_track = Converter.hz_to_cent(pitch_track, ref_freq=tonic_freq)
+        pd = PitchDistribution.from_cent_pitch(cent_track, tonic_freq,
+                                               self.smooth_factor,
+                                               self.step_size)
+        pcd_vals = pd.to_pcd().vals
+        mode_est = self.neural_net.estimate(pcd_vals)
+        max_idx = np.argmax(mode_est)
+        return [1 if(idx == max_idx) else 0 for idx, _ in enumerate(mode_est)]
